a9ecaf7c4796c4151c4af678850fa47f46b137ae
Silvia Ferrai <silviafer@hotmail.it>
2023-01-06 19:19:27 +0100

    Create first stack with 3 pages

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {Text} from 'react-native';
+import {StackNavigator} from './src/navigator/StackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Text>App Screen</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
