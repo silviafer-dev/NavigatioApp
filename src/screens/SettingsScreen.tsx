d3fab5595a6d2eddbb8ac49c979ac4f8acf2dff8
Silvia Ferrai <silviafer@hotmail.it>
2023-01-10 20:00:35 +0100

    Use of useSafeAresInserts hook

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {styles} from '../theme/appTheme';
 
 export const SettingsScreen = () => {
+  const insets = useSafeAreaInsets();
+
+  //Hook para crear un header y en Ios el titulo no se quede pegado al top
+  // No es un problema en android
   return (
-    <View>
+    <View style={{...styles.globalMargin, marginTop: insets.top + 20}}>
       <Text>Settings Screen</Text>
     </View>
   );
