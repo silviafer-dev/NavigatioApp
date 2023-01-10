16fc8d7a90f4da2bbdbdad2b90e88b13d7eb8a8f
Silvia Ferrai <silviafer@hotmail.it>
2023-01-10 17:33:19 +0100

    Install drawer and react-reanimated, create new component MenuLateral for the settings app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {StackNavigator} from './src/navigator/StackNavigator';
+import {MenuLateralBasico} from './src/navigator/MenuLateralBasico';
+// import {StackNavigator} from './src/navigator/StackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <MenuLateralBasico />
     </NavigationContainer>
   );
 };
