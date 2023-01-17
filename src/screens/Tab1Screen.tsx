4a09eda4f7a413686eed1ac14d848cd764ee0cc1
Silvia Ferrai <silviafer@hotmail.it>
2023-01-17 15:46:29 +0100

    Install icons

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,14 +1,27 @@
 import React, {useEffect} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {Text, View} from 'react-native';
+import {styles, colors} from '../theme/appTheme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export default function Tab1Screen() {
+  const {top} = useSafeAreaInsets();
   useEffect(() => {
     console.log('Tab1Screen');
   }, []);
 
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={{...styles.globalMargin, marginTop: top + 20}}>
+      <Text style={styles.title}> Iconos </Text>
+      <Text>
+        <Icon name="airplane-outline" size={50} color={colors.primary} />
+        <Icon name="male-female-outline" size={50} color={colors.primary} />
+        <Icon name="skull" size={50} color={colors.primary} />
+        <Icon name="star-half-outline" size={50} color={colors.primary} />
+        <Icon name="water-outline" size={50} color={colors.primary} />
+        <Icon name="logo-octocat" size={50} color={colors.primary} />
+        <Icon name="logo-instagram" size={50} color={colors.primary} />
+      </Text>
     </View>
   );
 }
